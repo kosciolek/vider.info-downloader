b8c30617d5d722c39937c77ed22e4e92b842b2d0
kosciolek <45483493+kosciolek@users.noreply.github.com>
2021-07-12 11:32:45 +0200

    eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,6 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   extends: ["airbnb-typescript/base", "prettier"],
   env: {
-    commonjs: true,
-    es6: true,
     node: true,
   },
   parserOptions: {
@@ -15,5 +13,4 @@ module.exports = {
     "import/prefer-default-export": "off",
     "no-underscore-dangle": "off",
   },
-  plugins: ["@typescript-eslint"],
 };
